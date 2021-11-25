eb532e0ec03558889c5d3b0e2543b44af646c37a
Sarath Chandra <91371908+sarath1405@users.noreply.github.com>
2021-11-25 19:10:04 +0530

    commit2

--- main.js
+++ main.js
@@ -7,24 +7,26 @@ var arr = [];
  
 button1.addEventListener('click', () =>{
     if(input.value === ''){
-        alert('enter something..!');
+        alert('Item name cant be empty..!');
     }
-    arr.push(input.value);
-    var node = document.createElement('li');
-    var del = document.createElement("button");
-    del.innerHTML = '<i class="fas fa-trash" style="color:black; font-size:20px" ></i>';
-    node.appendChild(document.createTextNode(arr[arr.length-1]));
-    list.appendChild(node);
-    node.setAttribute('id',k);
-    del.setAttribute('class','delete-items');
-    k++;
-    var id;
-    del.addEventListener('click',()=>{
-        id = parseInt(node.id);
-        node.parentNode.removeChild(node);
-    },false);
-    node.appendChild(del);
-    input.value = '';
+    else {
+        arr.push(input.value);
+        var node = document.createElement('li');
+        var del = document.createElement("button");
+        del.innerHTML = '<i class="fas fa-trash" style="color:black; font-size:20px" ></i>';
+        node.appendChild(document.createTextNode(arr[arr.length-1]));
+        list.appendChild(node);
+        node.setAttribute('id',k);
+        del.setAttribute('class','delete-items');
+        k++;
+        var id;
+        del.addEventListener('click',()=>{
+            id = parseInt(node.id);
+            node.parentNode.removeChild(node);
+        },false);
+        node.appendChild(del);
+        input.value = '';
+    }
 })
 
 
